6bb7122826c93ec810e93149670b72b04dabc655
Rohit <deoria88400@gmail.com>
2026-04-25 17:30:50 +0530

    out of stock update

--- jobs/expiryJob.js
+++ jobs/expiryJob.js
@@ -2,14 +2,26 @@ const cron = require("node-cron");
 const Inventory = require("../models/inventory");
 
 function startExpiryJob() {
-    cron.schedule('0 * * * *', async() => {
+    cron.schedule('0 * * * *', async () => {
         const now = new Date();
 
-        await Inventory.updateMany(
-            { expiryDate: { $lte: now }, status: { $ne: "expired" } },
-            { $set: { status: "expired" } }
-        );
+        try {
+            // 1. Mark expired
+            await Inventory.updateMany(
+                { expiryDate: { $lte: now }, status: { $ne: "Expired" } },
+                { $set: { status: "Expired" } }
+            );
+
+            // 2. Mark out of stock (only if NOT expired)
+            await Inventory.updateMany(
+                { quantity: { $lte: 0 }, status: { $ne: "Expired" } },
+                { $set: { status: "Out of Stock" } }
+            );
+
+        } catch (err) {
+            console.error("Cron Job Error:", err);
+        }
     });
-};
+}
 
 module.exports = startExpiryJob;
